93fc86a5638e04cdd7517dbb29a07cb4475d26fd
Andrei <safin12345@yandex.ru>
2016-10-15 18:23:16 +0400

    xml

---
+++ PO43/Safin/wdad/learn/xml/TestXml.java
@@ -0,0 +1,24 @@
+package PO43.Safin.wdad.learn.xml;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Objects;
+
+class TestXml{
+
+    public static void main(String[] args){
+        try{
+            System.out.println("Test");
+        XmlTask test = new XmlTask();
+        //String date = "2012.03.23";
+           // Calendar calendar =Calendar.getInstance();
+           // DateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+
+
+        }
+        catch (Exception e){e.printStackTrace();}
+    }
+}
