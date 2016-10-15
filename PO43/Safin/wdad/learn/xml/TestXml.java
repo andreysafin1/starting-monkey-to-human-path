6d15a5cbf5beaa38a04d2025fb0b005a3c39d36a
Andrei <safin12345@yandex.ru>
2016-10-15 20:17:28 +0400

    xml

--- PO43/Safin/wdad/learn/xml/TestXml.java
+++ PO43/Safin/wdad/learn/xml/TestXml.java
@@ -1,14 +1,27 @@
 package PO43.Safin.wdad.learn.xml;
 
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import java.awt.print.Book;
 import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
+import java.io.Reader;
 
 public class TestXml{
-    public static void main(String[] args) throws IOException,SAXException,ParserConfigurationException,TransformerException {
-XmlTask List = new XmlTask();
+    public static void main(String[] args)  {
+        try {
+            XmlTask List = new XmlTask();
+            // NodeList Reader = null;
+            // Node Book = null;
+            List.removeBook(null, null);
+        }
+       catch (Exception e)
+       {
+           e.printStackTrace();
+       }
     }
 }
--- PO43/Safin/wdad/learn/xml/XmlTask.java
+++ PO43/Safin/wdad/learn/xml/XmlTask.java
@@ -3,29 +3,33 @@ package PO43.Safin.wdad.learn.xml;
 import java.awt.print.Book;
 import java.io.IOException;
 
-
+import java.io.File;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
-
-import java.io.File;
 import java.io.Reader;
 
 
 public class XmlTask {
-    private Document document;
-    private String path = "C:/Users/Андрей/IdeaProjects/starting-monkey-to-human-path-master/starting-monkey-to-human-path/PO43/Safin/wdad/learn/xml/Test.xml";
+    Document doc;
+    String path = "C:/JAVA/starting-monkey-to-human-path-master/starting-monkey-to-human-path/PO43/Safin/wdad/learn/xml/Test.xml";
 
-    public XmlTask() throws IOException, ParserConfigurationException, SAXException {
-        File file = new File(path);
-        document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(path);
-    }
-    private Node findReader(String firstName,String secondName) {
+  public XmlTask() throws IOException, ParserConfigurationException, SAXException {
+      File file = new File(path);
+      doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(path);
+  }
+
+
+
+/*    private Node findReader(String firstName,String secondName) {
         String tedReader = "reader";
         String attrFirstName = "firstname";
         String attrSecondName = "secondname";
@@ -40,23 +44,35 @@ public class XmlTask {
             return node;
         }
         return null;
+    }*/
+
+
+    public void removeBook(Reader reader, Book book) throws TransformerException {
+
+        String tagReader = "reader";
+        String tagBook = "book";
+        NodeList Reader = doc.getElementsByTagName(tagReader);
+        NodeList Book = doc.getElementsByTagName(tagBook);
+        NamedNodeMap attributes;
+        Node node = null;
+        for (int i = 0; i < Reader.getLength(); i++) {
+            attributes = Reader.item(i).getAttributes();
+            if (attributes.getNamedItem(tagBook).getNodeValue().equals(Book)
+                    && attributes.getNamedItem(tagReader).getNodeValue().equals(Reader)) ;
+            attributes.removeNamedItem(tagBook);
+            writeDoc();
+        }
     }
 
-
-public void removeBook(Reader reader,Book book) throws TransformerException
-{
-    try {
-        NodeList tagReader = document.getElementsByTagName("reader");
-        NodeList tagBook = document.getElementsByTagName("book");
+    private void writeDoc() throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource domSource = new DOMSource(doc);
+        StreamResult streamResult = new StreamResult(new File(path));
+        transformer.transform(domSource, streamResult);
     }
- catch (NullPointerException e)
- {
-     System.out.println("Такой записи нет");
- }
-
-
 }
-}
+
 
 
 
